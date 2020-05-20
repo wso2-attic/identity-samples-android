a26f34e05272a4f2f1e54a936aeaab9a50da3d43
piraveena <piraveena18@gmail.com>
2020-05-20 17:33:17 +0530

    Refactor

--- app/src/main/java/org/wso2/oidc/sample/UserInfoActivity.java
+++ app/src/main/java/org/wso2/oidc/sample/UserInfoActivity.java
@@ -1,16 +1,33 @@
-package com.example.myapplication;
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.oidc.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.example.sample.R;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import org.oidc.agent.sso.LoginService;
 import org.oidc.agent.sso.OAuth2TokenResponse;
